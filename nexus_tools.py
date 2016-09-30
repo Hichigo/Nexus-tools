45876a9ac0647788e0ffe9cf27458dcd214dfd18
hichigo <hichigo91@gmail.com>
2016-09-30 15:41:55 +0300

    add description bl_info

--- nexus_tools.py
+++ nexus_tools.py
@@ -1,56 +1,75 @@
+# bl_info
+bl_info = {
+	"name": "Nexus tools",
+	"author": "nexus studio",
+	"version": (0,0,1),
+	"blender": (2,77),
+	"location": "T > Nexus Tools",
+	"description": "Explode meshes",
+	"warning": "",
+	"wiki_url": "",
+	"categoy": "Mesh"
+}
+
 import bpy
 from bpy.props import *
 
-class LayoutDemoPanel(bpy.types.Panel):
-    """Creates a Panel in the scene context of the properties editor"""
-    bl_label = "Nexus Tools"
-    bl_idname = "nexustoolsid"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'TOOLS'
-    bl_category = "Nexus Tools"
-    bl_context = "objectmode"
-    
-    bpy.types.Scene.explodeX = FloatProperty(
-        name = "X",
-        min = 0,
-        default = 0.0,
-        description = "Explode meshes by X coordinate"
-    )
-    
-    bpy.types.Scene.explodeY = FloatProperty(
-        name = "Y",
-        min = 0,
-        default = 0.0,
-        description = "Explode meshes by Y coordinate"
-    )
-    
-    bpy.types.Scene.explodeZ = FloatProperty(
-        name = "Z",
-        min = 0,
-        default = 0.0,
-        description = "Explode meshes by Z coordinate"
-    )
 
-    def draw(self, context):
-        layout = self.layout
-        
-        box = layout.box()
-        
-        box.label(text="Explode objects")
-        
-        col = box.column(align=True)
-        col.prop(context.scene, "explodeX")
-        col.prop(context.scene, "explodeY")
-        col.prop(context.scene, "explodeZ")
+
+#class panel
+class NexusToolsPanel(bpy.types.Panel):
+	"""Creates a Panel in the view3d context of the tools panel (key "T")"""
+	bl_label = "Nexus Tools"
+	bl_idname = "nexustoolsid"
+	bl_space_type = 'VIEW_3D'
+	bl_region_type = 'TOOLS'
+	bl_category = "Nexus Tools"
+	bl_context = "objectmode"
+	
+	offsetX = bpy.types.Scene.explodeX = FloatProperty(
+		name = "X",
+		min = 0,
+		default = 0.0,
+		description = "Explode meshes by X coordinate"
+	)
+	
+	offsetY = bpy.types.Scene.explodeY = FloatProperty(
+		name = "Y",
+		min = 0,
+		default = 0.0,
+		description = "Explode meshes by Y coordinate"
+	)
+	
+	offsetZ = bpy.types.Scene.explodeZ = FloatProperty(
+		name = "Z",
+		min = 0,
+		default = 0.0,
+		description = "Explode meshes by Z coordinate"
+	)
+
+	def draw(self, context):
+		layout = self.layout
+		
+		box = layout.box()
+		
+		box.label(text="Explode objects")
+		
+		col = box.column(align=True)
+		col.prop(context.scene, "explodeX")
+		col.prop(context.scene, "explodeY")
+		col.prop(context.scene, "explodeZ")
+
+	def execute(self, context):
+		bpy.context.object.location.x += offsetX
 
 
 def register():
-    bpy.utils.register_class(LayoutDemoPanel)
+	bpy.utils.register_class(LayoutDemoPanel)
 
 
 def unregister():
-    bpy.utils.unregister_class(LayoutDemoPanel)
+	bpy.utils.unregister_class(LayoutDemoPanel)
 
 
 if __name__ == "__main__":
-    register()
+	register()
